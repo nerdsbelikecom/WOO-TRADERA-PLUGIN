f8b649532cc31d0d2cdf0d90e64edabcda151907
nerdsbelike.com <info@nerdsbelike.com>
2024-08-18 14:07:36 +0200

    Delete wooTraderaPlugin/includes directory

--- wooTraderaPlugin/includes/views/admin/view-admin-settings.php
+++
@@ -1,5 +0,0 @@
-<!-- This is a placeholder for your admin settings page HTML -->
-<div class="wrap">
-    <h1><?php _e('Tradera API Settings', 'woo-tradera-plugin'); ?></h1>
-    <!-- Your settings form will go here -->
-</div>
--- wooTraderaPlugin/includes/views/public/view-public-display.php
+++
@@ -1,4 +0,0 @@
-<!-- This is a placeholder for your frontend display -->
-<div class="woo-tradera-public">
-    <!-- Your public-facing content will go here -->
-</div>
